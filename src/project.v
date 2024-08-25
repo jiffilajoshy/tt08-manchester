56e5f4c4a84691a12f614f8f465876b24d43d940
jiffilajoshy <67942508+jiffilajoshy@users.noreply.github.com>
2024-08-25 09:52:04 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,7 +20,8 @@ module tt_um_manchester (
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  assign [7:0] ui_in =0,  
+  assign [7:0] uo_out =0,
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
